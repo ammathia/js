a78e8a56db57a172d7d7eeae492a59056093d577
Grosul Igor <grosuligor99@mail.ru>
2022-12-22 11:27:39 +0200

    1

---
+++ app.js
@@ -0,0 +1,91 @@
+/*
+let x = 0;
+
+let myArray1 = ["white", "#e6ccff", "#d9b3ff", "#cc99ff", "#bf80ff", "#b366ff", "#a64dff", "#9933ff", "#8c1aff"];
+
+
+let y = 0;
+function addOne() {
+    x = x + 1;
+    document.getElementById('valuePara').innerHTML = ("Count: " + x);
+    y = y + 1;
+    let colorArr1 = myArray1[y];
+    document.body.style.backgroundColor = colorArr1;
+
+}
+
+
+function subOne() {
+    x = x - 1;
+    document.getElementById('valuePara').innerHTML = ("Count: " + x);
+    y = y - 1;
+    let colorArr1 = myArray1[y];
+    document.body.style.backgroundColor = colorArr1;
+}
+
+
+function showArray() {
+    document.getElementById("show").innerHTML = myArray1;
+}
+
+let obj = "max";
+
+let myObject = {
+    0: "Vasya",
+    [obj + "Speed"]: 12,
+    height: 1,
+};
+
+let first = myObject.wheels = 4;
+
+function showObject() {
+    let value = myObject.wheels;
+    document.getElementById("showObject").innerHTML = myObject.wheels;
+}
+
+console.log(myObject[0]);
+console.log(myObject["0"]);
+
+
+
+let Example_1 = Symbol('id');
+
+
+alert(Example_1.toString());
+console.log(Example_1);
+
+*/
+
+
+/*let correctName = "admin";
+let correctPassword = "password";
+
+
+let currentBalance = 1000;
+
+function testName() {
+    let attemptName = document.getElementById('formName').value;
+
+    let attemptPassword = document.getElementById('formPass').value;
+
+    let attemptBalance = document.getElementById('withdrawal').value;
+
+    let newBalance = currentBalance - attemptBalance;
+
+    if (attemptName != correctName) {
+        alert("Incorrect name");
+    } else if (attemptPassword != correctPassword) {
+        alert("Incorrect password");
+    } else if (attemptBalance > currentBalance) {
+        alert("Insufficient funds");
+    } else if (attemptBalance < 0) {
+        alert("Incorrect value of Withdrawal");
+    } else {
+        document.getElementById('Info').innerHTML = 'Your balance:' + newBalance;
+    }
+
+}*/
+
+
+
+
